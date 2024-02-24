a5a20cddb5ff7d0b5b1ecc858054b70a775d8f1a
Norman Koch <norman.koch@tu-dresden.de>
2024-02-24 15:06:51 +0100

    fix

---
+++ image.php
@@ -0,0 +1,28 @@
+<?php
+
+$images_path = "/docker_images/";
+
+if (is_dir($images_path)) {
+	chdir($images_path);
+}
+
+$filepath = $_GET["path"];
+
+if (preg_match("/\.\./", $filepath)) {
+	echo "Nice try";
+	exit(1);
+}
+
+$contents = file_get_contents($filepath);
+
+$expires = 14 * 60*60*24;
+
+header("Content-Type: image/jpeg");
+header("Content-Length: " . strlen($contents));
+header("Cache-Control: public", true);
+header("Pragma: public", true);
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT', true);
+
+echo $contents;
+exit;
+?>
