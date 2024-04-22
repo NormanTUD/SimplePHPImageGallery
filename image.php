45a2388dad259a5f4c371e2281ac1c2926b7f999
Norman Koch <norman.koch@tu-dresden.de>
2024-04-22 16:40:19 +0200

    fix

--- image.php
+++ image.php
@@ -14,17 +14,22 @@ if (preg_match("/\.\./", $filepath)) {
 }
 
 if(file_exists($filepath)) {
-	$contents = file_get_contents($filepath);
-	$expires = 14 * 60*60*24;
+	if(is_file($filepath)) {
+		$contents = file_get_contents($filepath);
+		$expires = 14 * 60*60*24;
 
-	header("Content-Type: image/jpeg");
-	header("Content-Length: " . strlen($contents));
-	header("Cache-Control: public", true);
-	header("Pragma: public", true);
-	header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT', true);
+		header("Content-Type: image/jpeg");
+		header("Content-Length: " . strlen($contents));
+		header("Cache-Control: public", true);
+		header("Pragma: public", true);
+		header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT', true);
 
-	echo $contents;
-	exit;
+		echo $contents;
+		exit;
+	} else {
+		echo "File is a folder";
+		exit(2);
+	}
 } else {
 	echo "File not found";
 	exit(1);
