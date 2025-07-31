ec2ab43a4ea7e637a088416eb7bc8cfa950ea651
Norman <norman@optimalbliss.de>
2025-07-31 13:41:25 +0200

    disable long click

--- functions.js
+++ functions.js
@@ -150,7 +150,7 @@ function onFolderMouseUp(e){
 	removeProgressBar();
 
 	var d = new Date();
-	var long_click = (d.getTime() - select_folder_timer) > 1000;
+	var long_click = false; // (d.getTime() - select_folder_timer) > 1000;
 	if (long_click || enabled_selection_mode){
 		e.preventDefault();
 		var container = e.target.closest('.thumbnail, .thumbnail_folder');
